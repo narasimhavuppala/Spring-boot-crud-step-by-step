cafcbaa95910f29edad47d46dcf20b5782b0a184
narasimhavuppala <nraovuppala@Wgmail.com>
2018-08-02 10:52:07 +0530

    Changed context path of application. fixed pathvariable

--- src/main/java/com/university/crud/springbootcrudrest/controller/StudentController.java
+++ src/main/java/com/university/crud/springbootcrudrest/controller/StudentController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.university.crud.springbootcrudrest.model.Student;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
-	@GetMapping("/:id")
+	@GetMapping("/{id}")
 	public Student getStudent(@PathVariable("id") String id) {
 
 		return new Student();
@@ -28,7 +30,7 @@ public class StudentController {
 		return obj;
 	}
 
-	@DeleteMapping("/:id")
+	@DeleteMapping("/{id}")
 	public void deleteStudent(@PathVariable("id") int id) {
 
 	}
